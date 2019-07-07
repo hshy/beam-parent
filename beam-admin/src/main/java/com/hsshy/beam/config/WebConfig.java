184e4e5bf0863911a6f200c908abc909aff40fab
hsshy <457030599@qq.com>
2019-07-07 20:52:17 +0800

    bug修改

--- beam-admin/src/main/java/com/hsshy/beam/config/WebConfig.java
+++ beam-admin/src/main/java/com/hsshy/beam/config/WebConfig.java
@@ -11,10 +11,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.util.Properties;
 @Configuration
-public class WebConfig extends WebMvcConfigurationSupport {
+public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private BeamAdminProperties beamAdminProperties;
