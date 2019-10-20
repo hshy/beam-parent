72b8e16c2c0c7d923fcfcc6008b2114f8b4f1074
hsshy <457030599@qq.com>
2019-10-20 14:35:19 +0800

    all

--- beam-admin/src/main/java/com/hsshy/beam/config/MybatisPlusConfig.java
+++ beam-admin/src/main/java/com/hsshy/beam/config/MybatisPlusConfig.java
@@ -1,13 +1,8 @@
 package com.hsshy.beam.config;
-
-import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 /**
  * MybatisPlus配置
@@ -27,20 +22,7 @@ public class MybatisPlusConfig {
         return new PaginationInterceptor();
     }
 
-    /**
-     * 逻辑删除插件
-     */
-    @Bean
-    public ISqlInjector sqlInjector() {
-        return new LogicSqlInjector();
-    }
 
-    /**
-     * SQL执行效率插件
-     */
-    @Bean
-    @Profile({"local"})// 设置 local 环境开启
-    public PerformanceInterceptor performanceInterceptor() {
-        return new PerformanceInterceptor();
-    }
+
+
 }
--- html/beam-manage-system/src/components/common/Header.vue
+++ html/beam-manage-system/src/components/common/Header.vue
@@ -15,7 +15,10 @@
                 </div>
 
                 <!-- 用户头像 -->
-                <div class="user-avator"><img :src="sysuser.avatar"></div>
+                <div class="user-avator">
+                    <img v-if="sysuser.avatar" :src="sysuser.avatar">
+                    <img v-else src="/static/img/user-head-img.png">
+                </div>
                 <!-- 用户名下拉菜单 -->
                 <el-dropdown class="user-name" trigger="click" @command="handleCommand">
                     <span class="el-dropdown-link">
